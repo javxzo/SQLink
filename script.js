f89699c6114c8916e6e3e7ba676f37fff616850e
Javeria <javeriafalam@gmail.com>
2024-05-26 00:45:30 -0400

    Altered solve option

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     const editorOptions = {
       lineNumbers: true,
       mode: "text/x-sql",
       theme: "default",
     };
 
-    // Initialize CodeMirror for the first textarea
-    const investigateTextarea = document.querySelector('.section:nth-of-type(1) .code-input');
-    const investigateEditor = CodeMirror.fromTextArea(investigateTextarea, editorOptions);
+    // CodeMirror for the first text area
+    const investigateTextarea = document.querySelector(
+      ".section:nth-of-type(1) .code-input"
+    );
+    const investigateEditor = CodeMirror.fromTextArea(
+      investigateTextarea,
+      editorOptions
+    );
 
-    // Initialize CodeMirror for the third textarea
-    const solveTextarea = document.querySelector('.section:nth-of-type(3) .code-input');
-    const solveEditor = CodeMirror.fromTextArea(solveTextarea, editorOptions);
-});
+    //remove later - if solve not added back
+    const solveTextarea = document.querySelector(
+      ".section:nth-of-type(3) .code-input"
+    );
+    const solveEditor = CodeMirror.fromTextArea(
+      solveTextarea,
+      editorOptions
+    );
+  });
